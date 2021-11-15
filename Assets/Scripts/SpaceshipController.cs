d5565584a775bfa2d3bed161a092756d51e4611b
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-11-15 12:55:05 +0200

    Player Movement

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -5,6 +5,10 @@ using UnityEngine.InputSystem;
 
 public class SpaceshipController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 25f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 3f;
+
     PlayerInput playerInput;
 
     float xThrow;
@@ -33,9 +37,17 @@ public class SpaceshipController : MonoBehaviour
 
     void Update()
     {
-        float newXPos = transform.localPosition.x + xThrow;
-        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
-        Debug.Log(transform.localPosition);
+        handleMovement();
+    }
+
+    void handleMovement(){
+        float newXPos = transform.localPosition.x + xThrow * Time.deltaTime * moveSpeed;
+        float newYPos = transform.localPosition.y + yThrow * Time.deltaTime * moveSpeed;
+
+        newXPos = Mathf.Clamp(newXPos, -xRange, xRange);
+        newYPos = Mathf.Clamp(newYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
     }
 
     void OnMovementInput(InputAction.CallbackContext context){
