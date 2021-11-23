0257cdd6a8332813f2e799420c5808a3a10c6d1b
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-11-23 21:34:01 +0200

    Reload Scene on Collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
-{
-    
-    void OnCollisionEnter(Collision collision)
+{   
+    [SerializeField] float loadDelay = 1f;
+
+    SpaceshipController movement;
+
+    bool isTransitioning = false;
+
+    void Awake()
     {
-        Debug.Log(this.name + " collided with " + collision.gameObject.name);
+        movement = GetComponent<SpaceshipController>();
     }
 
     void OnTriggerEnter(Collider collider)
     {
+        if(isTransitioning) return;
         Debug.Log($"{this.name} triggered by {collider.gameObject.name}");
+        crashSequence();
+    }
+
+    void crashSequence()
+    {
+        isTransitioning = true;
+        Invoke("reloadScene", loadDelay);
+        movement.enabled = false;
+    }
+
+    void reloadScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex);
     }
 }
