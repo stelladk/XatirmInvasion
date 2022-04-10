a36482a0440986683bdfe3647fe6486b8c8b8f51
Stella Douka <35050451+stelladk@users.noreply.github.com>
2022-04-10 22:19:18 +0300

    Singleton MusicPlayer

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayers > 1){
+            Destroy(gameObject);
+        }else{
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
