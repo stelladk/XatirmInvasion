16257960a3c52ccab1da70282543819cfe496c11
Stella Douka <35050451+stelladk@users.noreply.github.com>
2022-04-10 18:25:04 +0300

    Refactor code

--- Assets/Scripts/Custom Tracks/DialogueBehaviour.cs
+++ Assets/Scripts/Custom Tracks/DialogueBehaviour.cs
@@ -11,53 +11,30 @@ public class DialogueBehaviour : PlayableBehaviour
 {
     public DialoguePanel panel;
     public string speechText = "";
+    public bool pause = false;
 
     public AudioClip sound;
     public float volume = 1;
 
-    Image background;
-    TMP_Text dialogue;
-    AudioSource speeker;
-    Image radioImage;
     bool hasPlayed = false;
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
         // Get Track data
         panel = playerData as DialoguePanel;
-        background = panel.background;
-        dialogue = panel.dialogue;
-        speeker = panel.speeker;
-        radioImage = speeker.gameObject.GetComponent<Image>();
-
-        if(speechText != ""){
-            background.enabled = true;
-            dialogue.text = speechText;
-        }else{
-            background.enabled = false;
-            dialogue.text = "";
-        }
-
-        if(sound != null && !speeker.isPlaying && !hasPlayed){
+        
+        if (!hasPlayed){
+            panel.createDialogue(speechText, sound, volume);
             hasPlayed = true;
-            speeker.PlayOneShot(sound, volume);
         }
     }
 
     public override void OnBehaviourPlay(Playable playable, FrameData info)
     {
-        if(panel != null) {
-            background.enabled = true;
-            radioImage.enabled = true;
-            dialogue.text = "";
-        }
+
     }
     public override void OnBehaviourPause(Playable playable, FrameData info)
     {
-        if(panel != null) {
-            background.enabled = false;
-            radioImage.enabled = false;
-            dialogue.text = "";
-        }
+        if(panel != null) panel.endDialogue();
     }
 }
--- Assets/Scripts/DialoguePanel.cs
+++ Assets/Scripts/DialoguePanel.cs
@@ -10,5 +10,38 @@ public class DialoguePanel : MonoBehaviour
     public TMP_Text dialogue;
     public AudioSource speeker;
 
+    Image radioImage;
+
+    public void Awake()
+    {
+        radioImage = speeker.gameObject.GetComponent<Image>();
+
+        background.enabled = false;
+        radioImage.enabled = false;
+        dialogue.text = "";
+    }
+
+    public void createDialogue(string speechText, AudioClip sound, float volume)
+    {   
+        if(speechText != ""){
+            background.enabled = true;
+            radioImage.enabled = true;
+            dialogue.text = speechText;
+        }else{
+            background.enabled = false;
+            dialogue.text = "";
+        }
+
+        if(sound != null && !speeker.isPlaying){
+            speeker.PlayOneShot(sound, volume);
+        }
+    }
+
+    public void endDialogue()
+    {
+        background.enabled = false;
+        radioImage.enabled = false;
+        dialogue.text = "";
+    }
 
 }
